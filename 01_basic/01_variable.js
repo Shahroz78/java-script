a21c28ae42292e736a8408f9795adfcd0467f426
shahroz khan <shahrozkhan280@gmail.com>
2025-02-04 23:44:24 +0500

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345
+let accountEmail = "shahrozkhan280@gmail.com"
+var accountPassword = "8811"
+accountCity = "okara"
+let accountState;
+//accountId = 34569 // not allowed
+
+accountEmail = "hchc@.gmail.com"
+accountPassword = "99090"
+accountCity = "lahore"
+/*
+perefer not to use var
+because in issue in blockscope and functionalscope
+*/
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
