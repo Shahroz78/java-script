a402c8e25fa0f3b9768e5bda8b5be437d1bd7d5e
shahroz khan <shahrozkhan280@gmail.com>
2025-02-04 23:36:44 +0500

    container config done

---
+++ 01_basic/01_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as a newer version
+// alert( 3 + 3) // we are using node js,not a browser
+
+console.log( 3 +
+    //code readability should be high
+3);
+console.log("shahroz khan");
+
+let name = "shahroz khan"
+let age = 24
+let isLoggedIn = true
+let state;
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean =>true/false
+// null => standalone value
+// undefined => value is not defined
+// symbol => unique
+// object
+console.log(typeof "shahroz khan")
+console.log(typeof age )
+console.log(typeof null) // object is output
+console.log(typeof undefined) //undefined is type is undefined
