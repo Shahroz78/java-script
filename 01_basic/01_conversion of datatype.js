c1966dc7daf90c61cc175a05d6e50522d4b84aca
shahroz khan <shahrozkhan280@gmail.com>
2025-02-07 09:50:36 +0500

    conversion of datatype

---
+++ 01_basic/01_conversion of datatype.js
@@ -0,0 +1,27 @@
+let score = "shahroz khan"
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Notes to remember js datatypes
+// "string" ==> NaN (not an number)
+// null     ==> 0
+//undefined ==> NaN
+// true    ==> 1, false ==> 0
+// "33abs" ==> NaN
+// "33"    ==>33
+let isLoggedIn = "hitesh"
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 ==> true
+// "" == false
+// "shahroz khan" ==> true (-- String ma jo bhi value hogi woh ose boolean ka lehaz sa true dekhay ga --)
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
