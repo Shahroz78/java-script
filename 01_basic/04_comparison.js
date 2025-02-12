faa99bba1b826c0b6ffc96c09bd68d112cffbba0
shahroz khan <shahrozkhan280@gmail.com>
2025-02-12 22:41:44 +0500

    comparison config  done

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log(2 > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// This types of conversion to create a confusion
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// (===)
+console.log("2" === 2)
+
+//clean code is the highest periorety
