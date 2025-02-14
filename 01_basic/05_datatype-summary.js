29e25a51e1ba018855a71a62f7c5efd26b557d0b
shahroz khan <shahrozkhan280@gmail.com>
2025-02-14 11:04:42 +0500

    datatype-summary config done

---
+++ 01_basic/05_datatype-summary.js
@@ -0,0 +1,40 @@
+// primitive data types
+
+// 7 Types: Strings, Number, Boolean, null, Undefined, Symbol,BigInt
+
+// MAster of js to learn in objects/web events
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+//const id = Symbol('123')
+//const anotherId = Symbol('123')
+// console.log( id === anotherId);
+
+//const bigNumber = 345688886564465777888n
+
+// n to convert BigInt
+// Reference (n0n premptive)
+
+// Array, Objects, Function
+const heros =['Hulk', 'Superman','captain Amarica']
+
+let myObj = {
+    name :"Shahroz khan",
+    age : 18,
+}
+
+const myFunction = function(){
+    console.log("hello World");
+}
+
+console.log(typeof heros);
+// bignumber is typeof (undefined)
+// Function is typeof   (Function)
+// myObj is a typeof (object)
+// anotherId is a type of (symbol)
+// heros is a type of (object)
+// Documentation link type of:
+// https://262.ecma-international.org/5.1/#sec-11.4.3
