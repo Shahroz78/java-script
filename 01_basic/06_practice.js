6866aaff4c79c775b440c5ff0a21f0b4555fc6d8
shahroz khan <shahrozkhan280@gmail.com>
2026-01-25 16:36:32 +0500

    container config done

---
+++ 01_basic/06_practice.js
@@ -0,0 +1,69 @@
+/*
+//question no 1:Write a program using var to store your name and print it on the console.
+var Name = "shahroz khan"
+console.log(Name)
+*/
+/*
+Question 2:Declare two numbers using let, add them, and display the result.
+let Number1 = 12
+let Number2 = 12
+let sum = Number1 + Number2;
+console.log("sum is :",sum);
+*/
+/*question no 3:Create a constant variable using const to store the value of PI and print it.
+const pi = 3.14;
+console.log(pi);
+*/
+//Question no 4:Write a program that declares a variable using var, then change its value and print both values.
+//var Name = "shahroz khan"
+//var Name = " shahzaib khan"
+//console.table(Name);
+/*var number = 5;
+console.log("Before change:", number);
+
+number = 10;
+console.log("After change:", number);
+*/
+//Question no5:Write a program using let inside a block { } and try to access it outside the block. Observe the result.
+ /*  var Name = "shah g";    
+    Name = "khan g";
+    console.log("before change :",Name);
+    console.log("after change :",Name);
+    //    
+{
+    let x = 10;
+    console.log("Inside block:", x);
+}
+
+console.log("Outside block:", x);
+*/
+//question no:6Declare a variable using let and update its value after declaration. Print the updated value.
+/*
+let accountId = 8811;
+accountId = 4561;
+console.log(accountId);
+*/
+//Question:7
+/*const BirthYear = 2000;
+console.log(BirthYear);
+*/
+//Question 7:Try to redeclare a variable using var in the same scope. Write code to show this behavior
+/*var a = 10;
+console.log("First value:", a);
+
+var a = 20;   // redeclaration allowed
+console.log("Second value:", a);
+*/
+//question 9:Try to redeclare a variable using let in the same scope. What happens? Write the code.
+/*let a = 10;
+console.log("First value:", a);
+
+let a = 20;   // redeclaration allowed
+console.log("Second value:", a);
+*/
+//question 10:Write a program that uses var, let, and const together to store a number, a name, and a fixed value, then print all of them.
+/*const accountId = 456721;
+let Number = "45/GD"
+var CityName = "Okara"
+console.log(accountId,Number,CityName);
+*/
