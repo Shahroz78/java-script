e2546ab373be91f9b1c2fa813f365d4350eace39
shahroz khan <shahrozkhan280@gmail.com>
2026-02-05 10:27:58 +0500

    container config done

---
+++ 01_basic/07_strin-practice.js
@@ -0,0 +1,25 @@
+//Question:1
+//Lenth of the strings
+let text = "hello"
+//console.log(text.length);
+//console.log(text.toUpperCase());
+
+//get character from strings
+let word = ["java","script"]
+//console.log(word.charAt(2));
+//console.log(word.charAt(6));
+
+// msg include strings
+let msg = "    I love Javascript    "
+//console.log(msg.includes("Javascript"));
+//console.log(msg.includes("python"));
+//console.log(msg.indexOf("Javascript"));
+//console.log(msg.slice('0','11'));
+//console.log(msg.replace("Javascript","python"));
+//console.log(msg.trim("I love Javascript"));
+//console.log(msg.split("-"));
+//console.log(msg.split(""));
+console.log(["java".concat("script")]);
+
+
+
